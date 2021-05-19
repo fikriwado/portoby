e35bf5f47ebf99758e825f246ca301d475ecccc6
fikriwado <fixwad.online@gmail.com>
2021-05-19 19:01:45 +0700

    header menu and dark mode, change favicon, and add selected works

--- src/components/header.js
+++ src/components/header.js
@@ -22,18 +22,29 @@ const Header = () => {
 		<header className="py-5">
 			<div className="container">
 				<div className="w-full lg:w-7/12 mx-auto lg:px-8">
-					<div className="flex flex-wrap items-center border-b-2 border-gray-300 py-7">
-						<StaticImage
-							src="../images/profile.png"
-							width={95}
-							quality={95}
-							formats={["AUTO", "WEBP", "AVIF"]}
-							className="rounded-full mr-6"
-							alt="profile"
-						/>
-						<div className="font-fira-code">
-							<h1 className="text-4xl font-bold mb-1"><Link to="/">{title}</Link></h1>
-							<p className="ml-0.5 text-gray-500">{specialist}</p>
+					<div className="flex flex-wrap items-center justify-between border-b-2 border-gray-300 py-7">
+						<div className="flex flex-wrap items-center ">
+							<StaticImage
+								src="../images/profile.png"
+								width={95}
+								quality={95}
+								formats={["AUTO", "WEBP", "AVIF"]}
+								className="rounded-full mr-6"
+								alt="profile"
+							/>
+							<div className="font-fira-code">
+								<h1 className="text-4xl font-bold mb-1"><Link to="/">{title}</Link></h1>
+								<p className="ml-0.5 text-gray-500">{specialist}</p>
+							</div>
+						</div>
+						<div className="flex flex-wrap items-center">
+							<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 ml-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+							</svg>
+							
+							<svg xmlns="http://www.w3.org/2000/svg" className="h-7 w-7 ml-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+								<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16M4 18h16" />
+							</svg>
 						</div>
 					</div>
 				</div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-
+import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
@@ -19,6 +19,7 @@ const IndexPage = () => (
 				</p>
 				<p>My official site is at <a href="https://fikriwado.com" className="text-pink-500 font-medium">https://fikriwado.com</a>.</p>
 				<p>For this website I named portoby, <a href="https://github.com/fikriwado/portoby" className="text-pink-500 font-medium">check the repository</a>. You can use this freely, it uses Tailwindcss and Gatsbyjs technology. You only need to fork then change the profile, then you can deploy on netlify easily.</p>
+				<Link to="/" className="block border-2 border-cool-gray-600 text-cool-gray-600 hover:bg-cool-gray-600 hover:text-white duration-300 py-2 px-4 no-underline mt-8 text-center">Selected Works</Link>
 			</div>
 		</div>
 	</Layout>
