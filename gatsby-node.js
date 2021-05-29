f0325f0c64cce4f9d46401abfeaf6ec71212ea00
fikriwado <fixwad.online@gmail.com>
2021-05-29 16:47:27 +0700

    blog paginate

--- gatsby-node.js
+++ gatsby-node.js
@@ -19,7 +19,7 @@ exports.createPages = async ({ graphql, actions }) => {
     paginate({
         createPage,
         items: data.allMarkdownRemark.nodes,
-        itemsPerPage: 5,
+        itemsPerPage: 3,
         pathPrefix: '/blog',
         component: path.resolve('./src/templates/posts.js')
     })
